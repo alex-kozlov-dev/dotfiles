0885eff1efc8a5ce43ad14c2a34b30d918d1ffdd
Alex Kozlov <alex.k@preply.com>
2020-06-16 11:14:58 +0300

    fix vscode plugins install

--- vscode/plugins.sh
+++ vscode/plugins.sh
@@ -3,6 +3,6 @@
 # https://www.peerlator.com/blog/MyDotfilesPart5/
 while read EXTENSION
 do
-  echo_info "Install VSCode Extension: $EXTENSION"
+  echo "Install VSCode Extension: $EXTENSION"
   code --install-extension $EXTENSION
 done < $HOME/.dotfiles/vscode/plugins.txt
