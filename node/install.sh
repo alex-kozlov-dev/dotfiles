445fc785d9eb0c21452c79f616113344d26212b8
Alex Kozlov <alex.k@preply.com>
2023-06-22 15:27:41 +0200

    fix nvm

--- node/install.sh
+++ node/install.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 nvm install --lts --latest-npm
 npm i -g yarn
