dcbc616dbb059936a5c44407a5ecaa9f9f530036
Alex Kozlov <alex.k@preply.com>
2023-06-22 09:37:32 +0200

    update

--- iterm2/config.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.dotfiles/iterm2/config"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
